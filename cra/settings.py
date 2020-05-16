775411cca25c4ab765c059ea0d793c6261a6f286
Mateus Both <mateusboth@outlook.com>
2020-05-15 23:00:03 -0300

    static url

--- cra/settings.py
+++ cra/settings.py
@@ -135,16 +135,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-if DEBUG:
-    #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATIC_URL = '/static/'
-    # STATICFILES_DIRS = [
-    #     os.path.join(BASE_DIR, "static"),
-    #     '/var/www/static/',
-    # ]
-    MEDIA_URL = '/media/'
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 # adicionados
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -164,4 +154,20 @@ STATIC_URL = '/static/'
 
 # Simplified static file serving. and reduced size
 # https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
