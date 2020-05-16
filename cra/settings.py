0c4689a01fa9f73bd990ca7c713b598e7677410e
Mateus Both <mateusboth@outlook.com>
2020-05-15 21:28:17 -0300

    comenta STATICFILES_DIRS e altera requeriments

--- cra/settings.py
+++ cra/settings.py
@@ -136,10 +136,10 @@ USE_TZ = True
 if DEBUG:
     #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATIC_URL = '/static/'
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, "static"),
-        '/var/www/static/',
-    ]
+    # STATICFILES_DIRS = [
+    #     os.path.join(BASE_DIR, "static"),
+    #     '/var/www/static/',
+    # ]
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
